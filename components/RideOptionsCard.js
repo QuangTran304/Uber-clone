b54f878b81729eb9c83decd6e9b75eccc03433ca
Quang Tran <nhatquang304@gmail.com>
2022-01-04 23:42:59 -0500

    update RideOptionsCard component

--- components/RideOptionsCard.js
+++ components/RideOptionsCard.js
@@ -1,10 +1,23 @@
-import { Text, View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { SafeAreaView, Text, TouchableOpacity, View } from 'react-native';
+import { Icon } from 'react-native-elements';
+import tw from 'twrnc';
 
 const RideOptionsCard = () => {
+  const navigation = useNavigation();
+
   return (
-    <View>
-      <Text>Pick a ride</Text>
-    </View>
+    <SafeAreaView style={tw`bg-white flex-grow`}>
+      <View>
+        <TouchableOpacity
+          onPress={() => navigation.navigate('NavigateCard')}
+          style={tw`absolute top-3 left-5 z-50 p-3 rounded-full`}
+        >
+          <Icon name='chevron-left' type='fontawesome' />
+        </TouchableOpacity>
+        <Text style={tw`text-center py-5 text-xl`}>Select a Ride</Text>
+      </View>
+    </SafeAreaView>
   );
 };
 
